beb6b942cecabe2d1b5222560983a069e3cf2c19
Piotr Kulczycki <piotr9833@gmail.com>
2024-12-08 11:26:58 +0100

    started data analysis

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,8 +1,24 @@
 import pandas as pd
+import csv
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv('DQN.csv')
+class dataSample:
+    type = "" # s(simple)/m(memory)/t(target)/f(memory+target)
+    size = 0
+    h = 0
+    w = 0
+    mean_time = 0
+    mean_samples = 0
+
+dataSamples = []
+
+with open('DQN.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for row in reader:
+     dataSamples.append(dataSample(type="s",h = int(row[0]),w = int(row[1])))
+
+
+
 
 # Access data in the DataFrame using column names or indexing
-print(df['time'])
 print(df.iloc[0])  
